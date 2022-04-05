48601f57cbf643c81db8ec57e74fea6c02222581
kimkoo23 <sedreon@gmail.com>
2022-04-06 00:12:50 +0900

    twitter-clone 셋팅
    firebase 설치

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB8MI3-mgD4rLROF4jAUllBhkVCtLkhXWs",
+    authDomain: "twitter-clone-ff375.firebaseapp.com",
+    projectId: "twitter-clone-ff375",
+    storageBucket: "twitter-clone-ff375.appspot.com",
+    messagingSenderId: "929338317748",
+    appId: "1:929338317748:web:075b8ef776dc8f1b6a70e1"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
