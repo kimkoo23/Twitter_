d5aa2c6b79c141c2d002c6c6516a88649c99512e
kimkoo23 <sedreon@gmail.com>
2022-04-18 21:37:19 +0900

    logout생성

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
+import React, {  useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "./Navigation";
+import Profile from "../routes/Profile";
 
 const AppRouter = ({isLoggedIn}) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
+            </Route>
+            <Route exact path="/Profile">
+              <Profile />
             </Route>
           </>
         ) : (
